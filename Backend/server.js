45b0f9d0a97fc170d0c8da3983343f9151527805
Russole <850905junior@gmail.com>
2024-11-12 23:35:19 +0800

    fix: Add cookieParse, cors, bodyParser

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,18 @@ const app = express();
 
 require('dotenv').config(); // Load .env file environmental variable
 
+const cors = require('cors')
+const bodyParser = require('body-parser') // Parse JSON data in the request
+const cookieParser = require('cookie-parser') // Parse and handle cookies in the request
+app.use(cors({
+    origin : ['http://localhost:3000'],
+    credentials : true // cross-origin requests are allowed to carry authentication information (such as cookies)
+})) // Middleware to handle cross-origin requests in CORS
+    // It means that requests from only these two origins will be allowed
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
 app.use('/api',require('./routes/authRoutes'))
 
 app.get('/', (req,res)=>res.send('My backend'))

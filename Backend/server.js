0d90ea55359b40c4f1e9179ed08de8ed06b70712
Russole <850905junior@gmail.com>
2024-11-12 23:20:07 +0800

    Prject backend setup: 1.Backend package install 2.Listen port successfully

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config(); // Load .env file environmental variable
+
+
+app.get('/', (req,res)=>res.send('My backend'))
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))

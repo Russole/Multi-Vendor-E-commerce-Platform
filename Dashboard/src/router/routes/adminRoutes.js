689990ba871644f2aea0dc05583c4f5b0270848c
Russole <850905junior@gmail.com>
2024-11-14 11:42:21 +0800

    feat: add a product category page to the dashboard

--- Dashboard/src/router/routes/adminRoutes.js
+++ Dashboard/src/router/routes/adminRoutes.js
@@ -1,9 +1,15 @@
 import { lazy } from "react";   
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))   
+const Category = lazy(()=> import('../../views/admin/Category'))
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
         element : <AdminDashboard/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/category',
+        element : <Category/> ,
         role : 'admin'
     }
 ]

9805db05a92acbbc6b81285009faddd83c486fa8
Russole <850905junior@gmail.com>
2024-11-14 15:08:40 +0800

    feat(frontend): allow the admin to view order details
    Add order detail page.
    Set order detail route.
    Link order page to order detail page.

--- Dashboard/src/router/routes/adminRoutes.js
+++ Dashboard/src/router/routes/adminRoutes.js
@@ -4,6 +4,7 @@ const Category = lazy(()=> import('../../views/admin/Category'))
 const Sellers = lazy(()=> import('../../views/admin/Sellers'))  
 const Orders = lazy(()=> import('../../views/admin/Orders')) 
 const PaymentRequest = lazy(()=> import('../../views/admin/PaymentRequest'))
+const OrderDetails = lazy(()=> import('../../views/admin/OrderDetails'))
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
@@ -29,5 +30,10 @@ export const adminRoutes = [
         path: 'admin/dashboard/payment-request',
         element : <PaymentRequest/> ,
         role : 'admin'
-    }
+    },
+    {
+        path: 'admin/dashboard/order/details/:orderId',
+        element : <OrderDetails/> ,
+        role : 'admin'
+    },
 ]

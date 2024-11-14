ff199a3466373eec3497adec1dcd675a5294463b
Russole <850905junior@gmail.com>
2024-11-14 12:23:39 +0800

    feat: Design seller page on dashboard

--- Dashboard/src/router/routes/adminRoutes.js
+++ Dashboard/src/router/routes/adminRoutes.js
@@ -1,7 +1,8 @@
 import { lazy } from "react";    
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))  
 const Category = lazy(()=> import('../../views/admin/Category'))
-const Orders = lazy(()=> import('../../views/admin/Orders')) 
+const Orders = lazy(()=> import('../../views/admin/Orders'))
+const Sellers = lazy(()=> import('../../views/admin/Sellers'))  
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
@@ -16,6 +17,11 @@ export const adminRoutes = [
     {
         path: 'admin/dashboard/orders',
         element : <Orders/> ,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/sellers',
+        element : <Sellers/> ,
         role : 'admin'
     }
 ]

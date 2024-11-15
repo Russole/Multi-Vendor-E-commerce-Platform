9fdc998f8065c29af9d37a2a118e234e98dc8301
Russole <850905junior@gmail.com>
2024-11-15 09:28:58 +0800

    feat: design orders and payments page
    Set up the route for order page.
    Set up the route for payment page.
    Add order and payments page.

--- Dashboard/src/router/routes/sellerRoutes.js
+++ Dashboard/src/router/routes/sellerRoutes.js
@@ -4,6 +4,8 @@ const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))
 const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))  
 const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts'))
 const Products = lazy(()=> import('../../views/seller/Products'))
+const Orders = lazy(()=> import('../../views/seller/Orders')) 
+const Payments = lazy(()=> import('../../views/seller/Payments'))
 export const sellerRoutes = [
     {
         path: '/',
@@ -28,6 +30,16 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard/products',
         element : <Products/>,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/orders',
+        element : <Orders/>,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/payments',
+        element : <Payments/>,
         ability : ['seller']
     }
 ]

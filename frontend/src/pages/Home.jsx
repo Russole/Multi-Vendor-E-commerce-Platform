08ddd19f8418288eee2b4718519929b9801d8d75
Russole <850905junior@gmail.com>
2024-11-21 11:26:41 +0800

    feat: set up the design for the featured product

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Categorys from '../components/Categorys';
+import FeatureProducts from '../components/products/FeatureProducts';
 const Home = () => {
     return (
         <div className='w-full'>
             <Header/>
             <Banner/>
             <Categorys/>
+            <div className='py-[45px]'>
+            <FeatureProducts/>
+            </div>
         </div>
     );
 };

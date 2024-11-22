a78ac5cdb0280d987c8288ae9337ad4489a14f6f
Russole <850905junior@gmail.com>
2024-11-23 01:53:53 +0800

    feat: add product to cart

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -1,7 +1,9 @@
 import homeReducer from "./reducers/homeReducer";
+import cardReducer from "./reducers/cardReducer";
 import authReducer from "./reducers/authReducer";
 const rootReducer = {
     home: homeReducer,
-    auth: authReducer 
+    auth: authReducer,
+    card: cardReducer 
 }
 export default rootReducer;

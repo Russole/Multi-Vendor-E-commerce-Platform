685ac99d9eff5d05f3f98a996e716bfc7f6d6191
Russole <850905junior@gmail.com>
2024-11-22 10:26:07 +0800

    feat: fetch feature product and latest top rated discount

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,19 +6,21 @@ import FeatureProducts from '../components/products/FeatureProducts';
 import Products from '../components/products/Products';
 import Footer from '../components/Footer';
 import { useDispatch, useSelector } from 'react-redux';
-import { get_category } from '../store/reducers/homeReducer';
+import { get_category, get_products } from '../store/reducers/homeReducer';
 
 const Home = () => {
 
     console.log("rerender")
 
     const dispatch = useDispatch()
-    const {categorys} = useSelector(state => state.home)
     useEffect(() => {
         dispatch(get_category())
+        dispatch(get_products())
     },[])
-
+    const {categorys,products,latest_product,topRated_product,discount_product} = useSelector(state => state.home)
+    
     console.log(categorys)
+    console.log(products)
 
     return (
         <div className='w-full'>
@@ -26,20 +28,20 @@ const Home = () => {
             <Banner/>
             <Categorys categorys={categorys}/>
             <div className='py-[45px]'>
-            <FeatureProducts/>
+            <FeatureProducts products={products}/>
             </div>
             <div className='py-10'>
                 <div className='w-[85%] flex flex-wrap mx-auto'>
                     <div className='grid w-full grid-cols-3 md-lg:grid-cols-2 md:grid-cols-1 gap-7'>
             <div className='overflow-hidden'>
-                <Products title='Latest Product'/>
+                <Products title='Latest Product' products={latest_product}/>
             </div>
             
             <div className='overflow-hidden'>
-                <Products title='Top Rated Product'/>
+                <Products title='Top Rated Product' products={topRated_product}/>
             </div>
             <div className='overflow-hidden'>
-                <Products title='Discount Product'/>
+                <Products title='Discount Product' products={discount_product}/>
             </div>
                     </div> 
                 </div> 

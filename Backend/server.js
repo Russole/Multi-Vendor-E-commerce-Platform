2805f16adfdb36ab60d34b485c8d78398208cab8
Russole <850905junior@gmail.com>
2024-11-26 15:41:19 +0800

    feat: set up a chat system between the seller and the customer

--- Backend/server.js
+++ Backend/server.js
@@ -3,15 +3,50 @@ const app = express();
 
 require('dotenv').config(); // Load .env file environmental variable
 
+
+
 const cors = require('cors')
 const bodyParser = require('body-parser') // Parse JSON data in the request
 const cookieParser = require('cookie-parser') // Parse and handle cookies in the request
 const { dbConnect } = require('./utils/db')
+
+const socket = require('socket.io')
+const http = require('http')
+const server = http.createServer(app)
 app.use(cors({
     origin : ['http://localhost:3000'],
     credentials : true // cross-origin requests are allowed to carry authentication information (such as cookies)
 })) // Middleware to handle cross-origin requests in CORS
     // It means that requests from only these two origins will be allowed
+
+const io = socket(server, {
+    cors: {
+        origin: '*',
+        credentials: true
+    }
+})
+
+var allCustomer = []
+
+const addUser = (customerId,socketId,userInfo) => {
+    const checkUser = allCustomer.some(u => u.customerId === customerId)
+    if (!checkUser) {
+        allCustomer.push({
+            customerId,
+            socketId,
+            userInfo
+        })
+    }
+} 
+io.on('connection', (soc) => {
+    console.log('socket server running..')
+
+    soc.on('add_user',(customerId,userInfo)=>{
+        addUser(customerId,soc.id,userInfo)
+         
+   })
+})
+require('dotenv').config()
 
 app.use(bodyParser.json())
 app.use(cookieParser())
@@ -24,8 +59,9 @@ app.use('/api',require('./routes/dashboard/sellerRoutes'))
 app.use('/api',require('./routes/home/customerAuthRoutes'))
 app.use('/api',require('./routes/home/cardRoutes'))
 app.use('/api',require('./routes/order/orderRoutes'))
+app.use('/api',require('./routes/chatRoutes'))
+
 app.get('/', (req,res)=>res.send('Hello Server'))
 const port = process.env.PORT;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+server.listen(port, () => console.log(`Server is running on port ${port}`))
 dbConnect()
--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -3,11 +3,13 @@ import cardReducer from "./reducers/cardReducer";
 import authReducer from "./reducers/authReducer";
 import orderReducer from "./reducers/orderReducer";
 import dashboardReducer from "./reducers/dashboardReducer";
+import chatReducer from "./reducers/chatReducer";
 const rootReducer = {
     home: homeReducer,
     auth: authReducer,
     card: cardReducer,
     order: orderReducer,
-    dashboard: dashboardReducer
+    dashboard: dashboardReducer,
+    chat: chatReducer
 }
 export default rootReducer;

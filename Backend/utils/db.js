d5fd319fbd75f12d57a72f7afaf9a1af530f276b
Russole <850905junior@gmail.com>
2024-11-30 03:31:46 +0800

    Test CD workflow six time

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 module.exports.dbConnect = async()=>{
+    console.log("dbConnect")
     try {
         await mongoose.connect(process.env.DB_URL,{useNewURLParser: true})
         console.log("Database connected..")
